ef01c60c0f8d1102b2fd76ddea2f3a7354c976a5
jeanpaulsio <sio.jeanpaul@gmail.com>
2019-06-16 18:01:37 -0700

    prettier

--- lib/binary_search/square_root_test.rb
+++ lib/binary_search/square_root_test.rb
@@ -3,9 +3,7 @@ require_relative 'square_root'
 
 class SquareRootTest < Minitest::Test
   def test_cases
-    1.upto(20) do |i|
-      assert_equal sqrt_floored(i), SquareRoot.run(i)
-    end
+    1.upto(20) { |i| assert_equal sqrt_floored(i), SquareRoot.run(i) }
   end
 
   def sqrt_floored(i)
