d8c21e1171434b7d60a9abe9bd4614e8d132ad44
jeanpaulsio <sio.jeanpaul@gmail.com>
2019-07-01 23:12:11 -0700

    small bug

--- lib/arrays_and_strings/clone_even_numbers_test.rb
+++ lib/arrays_and_strings/clone_even_numbers_test.rb
@@ -4,7 +4,7 @@ require_relative 'clone_even_numbers'
 class CloneEvenNumbersTest < Minitest::Test
   def test_base_case
     expected = [1, 2, 2, 3, 4, 4, 5]
-    actual = CloneEvenNumbers.run([1, 2, 3, 4, 5, -1, 1])
+    actual = CloneEvenNumbers.run([1, 2, 3, 4, 5, -1, -1])
     assert_equal expected, actual
   end
 
