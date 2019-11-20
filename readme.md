45f598dcf2f96633d7ad52982ac5455ad4bf2363
JP Sio <sio.jeanpaul@gmail.com>
2019-11-20 00:08:05 -0800

    Renames to spec

--- readme.md
+++ readme.md
@@ -3,11 +3,3 @@
 ## Arrays and Strings
 
 1. [Clone Even Numbers](./lib/arrays_and_strings/clone_even_numbers.rb)
-
----
-
-## Running Tests
-
-```bash
-ruby test/arrays_and_strings/clone_even_numbers_test.rb
-```
--- spec/arrays_and_strings/clone_even_numbers_spec.rb
+++ spec/arrays_and_strings/clone_even_numbers_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require_relative '../test_helper'
+require_relative '../spec_helper'
 require_relative '../../lib/arrays_and_strings/clone_even_numbers'
 
 describe CloneEvenNumbers do
   it 'should work' do
     actual = CloneEvenNumbers.run([1])
-    assert_nil actual
+    expect(actual).must_be_nil
   end
 end
