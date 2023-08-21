064e25299a26d6b132545c08c353052b01bbe1be
jeanpaulsio <hello@jpsio.me>
2023-08-20 18:53:42 -0700

    Starting over for 2023

--- lib/arrays_and_strings/clone_even_numbers.rb
+++ lib/arrays_and_strings/clone_even_numbers.rb
@@ -5,26 +5,7 @@
 # Result: [1, 2, 2, 3, 4, 4]
 
 class CloneEvenNumbers
-  def self.find_last_positive_number(arr)
-    result = arr.find_index { |i| i == -1 }
-    result.nil? ? arr.length - 1 : result - 1
-  end
-
   def self.run(arr)
-    i = find_last_positive_number(arr)
-    j = arr.length
-
-    while i >= 0
-      if arr[i].even?
-        j -= 1
-        arr[j] = arr[i]
-      end
-
-      j -= 1
-      arr[j] = arr[i]
-      i -= 1
-    end
-
-    arr
+    # TODO
   end
 end
